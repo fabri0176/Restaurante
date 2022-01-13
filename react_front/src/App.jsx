6cc7940a66e185aa0a4f3f173bd6e23d5258317a
Nestor Parra <fabri0176@gmail.com>
2022-01-12 21:31:25 -0500

    React configuración rutas

---
+++ react_front/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigation } from './routes'
+
+export default function App() {
+    return (
+        <div>
+            <h1>App</h1>
+            <Navigation />
+        </div>
+    )
+}
