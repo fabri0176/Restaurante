66d380a87abb719f68ca48b895ecc9200c935ac6
Nestor Parra <fabri0176@gmail.com>
2022-01-16 15:47:51 -0500

    Confiuración rutas en read

--- react_front/src/routes/routes.client.js
+++ react_front/src/routes/routes.client.js
@@ -1,6 +1,6 @@
 import { ClientLayout } from "../layouts";
-import { Error404 } from "../pages";
 import { Home } from "../pages/Client";
+import { Error404 } from "../pages";
 
 
 const routesClient = [
@@ -8,6 +8,10 @@ const routesClient = [
         path: "/",
         layout: ClientLayout,
         component: Home,
+    },
+    {
+        layout: ClientLayout,
+        component: Error404
     }
 ];
 
--- react_front/src/routes/routes.js
+++ react_front/src/routes/routes.js
@@ -1,9 +1,16 @@
 import routesClient from "./routes.client";
 import routesAdmin from "./routes.admin";
+import { Error404 } from "../pages";
+import { BasicLayout } from "../layouts/BasicLayout";
+
 
 const routes = [
     ...routesAdmin,
-    ...routesClient
+    ...routesClient,
+    {
+        layout: BasicLayout,
+        component: Error404
+    }
 ];
 
 export default routes;
