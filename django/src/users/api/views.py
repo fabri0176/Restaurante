a7d8af826e53c104846474fba8c2abb2239a8e40
Nestor Parra <fabri0176@gmail.com>
2022-01-16 17:18:15 -0500

    Api crear usuario, encriptar contraseña

--- django/src/users/api/views.py
+++ django/src/users/api/views.py
@@ -1,6 +1,7 @@
 import imp
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from users.api.serializers import UserSerializer
@@ -10,3 +11,7 @@ class UserApiViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
